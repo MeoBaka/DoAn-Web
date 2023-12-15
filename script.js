fef93bc68f840947db51851b82b05f489b1a7448
Meow Baka <namconvn@gmail.com>
2023-12-15 11:45:17 +0700

    Update

--- script.js
+++ script.js
@@ -88,4 +88,124 @@ document.addEventListener("DOMContentLoaded",function(){
          }
 
     })
+    $(function() {
+    
+    //xử lý class cho noidung
+         $('.noidung').addClass('noidunghienra');
+
+         // xu ly class cho .momo
+         $('.momo').addClass('momodira');
+    // xu ly cho nut .nut click vao 
+    $('.dk').click(function(event) {
+         
+         //xử lý class cho noidung
+         $('.noidung').addClass('noidunghienra');
+
+         // xu ly class cho .momo
+         $('.momo').addClass('momodira');
+    });
+
+    //jquery cho nut close 
+    $('.nutdong, .momo').click(function(event) {
+         //xử lý class cho noidung
+         $('.noidung').removeClass('noidunghienra');
+
+         // xu ly class cho .momo
+         $('.momo').removeClass('momodira');
+
+        });  
+    });
+
+    var chaytrai = document.querySelector(".KhungContent");
+    console.log(chaytrai);
+
+    var vitritrai = chaytrai.offsetTop;
+    console.log(vitritrai);
+    window.addEventListener('scroll',function(){
+        console.log(window.pageYOffset);
+        if(window.pageYOffset >400 && window.pageYOffset <1520){
+              chaytrai.classList.add("trai");
+        }
+        else{
+            chaytrai.classList.remove("trai");
+        }
+    })
+
+    var chayphai = document.querySelector(".P");
+    console.log(chayphai);
+
+    var vitriphai = chayphai.offsetTop;
+    console.log(vitriphai);
+    window.addEventListener('scroll',function(){
+        console.log(window.pageYOffset);
+        if(window.pageYOffset >820 && window.pageYOffset <2320){
+              chayphai.classList.add("phai");
+        }
+        else{
+            chayphai.classList.remove("phai");
+        }
+    })
+
+    var chaylen = document.querySelector(".Khung-Store");
+    console.log(chaylen);
+
+    var vitrilen = chaylen.offsetTop;
+    console.log(vitrilen);
+    window.addEventListener('scroll',function(){
+        console.log(window.pageYOffset);
+        if(window.pageYOffset >1500 && window.pageYOffset <2820 ){
+              chaylen.classList.add("runup");
+        }
+        else{
+            chaylen.classList.remove("runup");
+        }
+    })
+    $(document).ready(function(){
+                        var currentIndex = 0;
+                        var slides = $('._1slide');
+
+                        // Hàm chuyển động tự động
+                        function autoSlide() {
+                            currentIndex = (currentIndex + 1) % slides.length;
+                            updateSlide();
+                        }
+
+                        // Hàm cập nhật hiển thị của slide
+                        function updateSlide() {
+                            var translateValue = -100 * currentIndex + '%';
+                            $('._1slide').css('transform', 'translateX(' + translateValue + ')');
+                        }
+
+                        // Thiết lập chuyển động tự động
+                        var interval = setInterval(autoSlide, 2000);
+
+                        // Sự kiện khi tương tác chuột vào slide
+                        $('._1slide').on('mouseenter', function() {
+                            clearInterval(interval); // Dừng chuyển động khi tương tác
+                        });
+
+                        // Sự kiện khi rời chuột khỏi slide
+                        $('._1slide').on('mouseleave', function() {
+                            interval = setInterval(autoSlide, 2000); // Khởi động lại chuyển động
+                        });
+
+                        // Sự kiện khi click vào nút trước
+                        $('.traiicon').click(function() {
+                            clearInterval(interval);
+                            currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+                            updateSlide();
+                            interval = setInterval(autoSlide, 2000);
+                        });
+
+                        // Sự kiện khi click vào nút tiếp theo
+                        $('.phaiicon').click(function() {
+                            clearInterval(interval);
+                            currentIndex = (currentIndex + 1) % slides.length;
+                            updateSlide();
+                            interval = setInterval(autoSlide, 2000);
+                        });
+                    });
+    
 })
+
+
